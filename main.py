637c0d3657ed10f46b9c1d596475415381952ae6
Alan Cofré <acofre2019@alu.uct.cl>
2025-04-17 23:32:54 -0400

    modificacion de main (el anterior era de prueba)

--- main.py
+++ main.py
@@ -1,9 +1,86 @@
+from fastapi import FastAPI, HTTPException, Depends
+from fastapi.middleware.cors import CORSMiddleware
+from sqlalchemy.orm import Session
 from database import init_db, SessionLocal
+from models import Vuelo, EstadoVuelo 
+from lista_vuelos import ListaVuelos
+from datetime import datetime
 
-# Inicializamos la base de datos y crea las tabla si no existen
+# Iniciar base de datos y app
 init_db()
+app = FastAPI()
+lista = None # Se creará al iniciar la app con la sesion
 
-# Confirmamos si funciona
-db = SessionLocal()
-print("Conexion exitosa a la base de datos")
-db.close
+# Obtener sesion 
+def get_db():
+    db = SessionLocal()
+    try:
+        yield db
+    finally:
+        db.close()
+
+# Inicializar ListaVuelos al iniciar
+@app.on_event("startup")
+def startup():
+    global lista
+    db = SessionLocal()
+    lista = ListaVuelos(db)
+
+# ENDPOINTS
+
+@app.post("/vuelos")
+def agregar_vuelo(vuelo: dict):
+    nuevo = Vuelo(
+        codigo=vuelo["codigo"],
+        estado=EstadoVuelo(vuelo["estado"]),
+        hora=datetime.fromisoformat(vuelo["hora"]),
+        origen=vuelo["origen"],
+        destino=vuelo["destino"]
+    )
+
+    if nuevo.estado == EstadoVuelo.emergencia:
+        lista.insertar_al_frente(nuevo)
+    else:
+        lista.insertar_al_final(nuevo)
+    return {"mensaje": "Vuelo agregado"}
+
+@app.get("/vuelos/total")
+def cantidad_vuelos():
+    return {"total": lista.longitud()}
+
+@app.get("/vuelos/proximo")
+def vuelo_primero():
+    vuelo = lista.obtener_primero()
+    if vuelo is None:
+        raise HTTPException(status_code=404, detail="Sin vuelos") 
+    return vuelo
+
+@app.get("/vuelos/ultimo")
+def vuelo_ultimo():
+    vuelo = lista.obtener_ultimo()
+    if vuelo is None:
+        raise HTTPException(status_code=404, detail="Sin vuelos") 
+    return vuelo
+
+@app.post("/vuelos/inserta")
+def insertar_en_posicion(vuelo: dict, posicion: int):
+    nuevo = Vuelo(
+        codigo=vuelo["codigo"],
+        estado=EstadoVuelo(vuelo["estado"]),
+        hora=datetime.fromisoformat(vuelo["hora"]),
+        origen=vuelo["origen"],
+        destino=vuelo["destino"]
+    )
+    lista.insertar_en_posicion(nuevo, posicion)
+    return {"mensaje": f"Vuelo insertado en posicion {posicion}"}
+
+@app.delete("/vuelo/extraer")
+def eliminar_vuelo(posicion: int):
+    vuelo = lista.extraer_de_posicion(posicion)
+    if vuelo is None:
+        raise HTTPException(status_code=404, detail="Posicion invalida")
+    return {"mensaje": f"Vuelo {vuelo.codigo} eliminado"}
+
+@app.get("/vuelos/lista")
+def listar():
+    return lista.listar_vuelos()
