d62aa63dc58e8ea8d9ad9deba87bac0e2cb8587f
Alan Cofré <acofre2019@alu.uct.cl>
2025-04-18 11:32:31 -0400

    schemas para añadir los vuelos y sus estados

---
+++ schemas.py
@@ -0,0 +1,19 @@
+from pydantic import BaseModel
+from datetime import datetime
+from enum import Enum
+
+class EstadoVueloEnum(str, Enum):
+    progamado = "programado"
+    emergencia = "emergencia"
+    retrasado = "retrasado"
+
+class VueloSchema(BaseModel):
+    codigo: str
+    estado: EstadoVueloEnum
+    hora: datetime
+    origen: str
+    destino: str
+
+
+
+
